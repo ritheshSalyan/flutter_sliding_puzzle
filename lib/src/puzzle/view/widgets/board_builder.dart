e3241ec3ae13d55ab2cc22744ddbbb6e8bf4e74b
ritheshSalyan <rithesh@dreamsoftin.com>
2022-01-23 19:55:03 +0530

    WIP on Drag

--- lib/src/puzzle/view/widgets/board_builder.dart
+++ lib/src/puzzle/view/widgets/board_builder.dart
@@ -1,34 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sliding_puzzle/src/puzzle/provider/board_controller.dart';
 
 import '../../puzzle.dart';
 
 class BoardView extends ConsumerWidget {
-  final PuzzleBoard board;
-  const BoardView({Key? key, required this.board}) : super(key: key);
-
+  const BoardView({
+    Key? key,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context, ref) {
-    return AspectRatio(
-      aspectRatio: 1,
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: board.xDim),
-        itemCount: board.tiles.length + 1,
-        itemBuilder: (context, index) {
-          final tile = board.tileForPos(_getX(index), _getY(index));
-          if (tile == null) return Container();
-          return InkWell(
-              onTap: () {
-                ref.read(PuzzelProvider.provider).moveTile(tile);
-                // log("Can Move ${tile.data} ${board.canMoveTile(tile)}");
-              },
-              child: TileBuilder(tile: tile));
-        },
+    final board = ref.watch(BoardLogicController.provider);
+    return Center(
+      child: SizedBox(
+        width: 500,
+        height: 500,
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: LayoutBuilder(builder: (context, constraints) {
+            final width = constraints.maxWidth;
+            final height = constraints.maxHeight;
+
+            const space = 30;
+
+            final tileWidth = (width / board.xDim);
+            // -(space * (board.xDim - 1));
+            final tileHeight = (height / board.yDim);
+            // -(space * (board.yDim - 1));
+
+            // return GridView.builder(
+            //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //       crossAxisCount: board.xDim),
+            //   itemCount: board.tiles.length + 1,
+            //   itemBuilder:
+            return Stack(
+              children: List<Widget>.from(board.tiles.map((tile) {
+                return TileBuilder(
+                  tile: tile,
+                  tileWidth: tileWidth,
+                  tileHeight: tileHeight,
+                );
+              })),
+            );
+            // );
+          }),
+        ),
       ),
     );
   }
-
-  int _getX(int index) => index ~/ board.xDim;
-  int _getY(int index) => index % board.xDim;
 }
--- lib/src/puzzle/view/widgets/tile_builder.dart
+++ lib/src/puzzle/view/widgets/tile_builder.dart
@@ -1,19 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sliding_puzzle/src/puzzle/extensions/extensions.dart';
 import 'package:sliding_puzzle/src/puzzle/model/model.dart';
+import 'package:sliding_puzzle/src/puzzle/provider/board_controller.dart';
 
-class TileBuilder extends StatelessWidget {
+import '../../puzzle.dart';
+
+class TileBuilder extends ConsumerStatefulWidget {
   final Tile tile;
-  const TileBuilder({Key? key, required this.tile}) : super(key: key);
+  const TileBuilder({
+    Key? key,
+    required this.tile,
+    required this.tileHeight,
+    required this.tileWidth,
+  }) : super(key: key);
+  final double tileHeight;
+  final double tileWidth;
 
   @override
+  ConsumerState<TileBuilder> createState() => _TileBuilderState();
+}
+
+class _TileBuilderState extends ConsumerState<TileBuilder> {
+  Offset extraOffset = Offset.zero;
+  MovablePosition _movablePosition = MovablePosition.none;
+
+  double get top =>
+      widget.tileHeight * widget.tile.currentPos.x + extraOffset.dy;
+  double get left =>
+      widget.tileWidth * widget.tile.currentPos.y + extraOffset.dx;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.red,
-        border: Border.all(color: Colors.blue),
-      ),
-      child: Center(
-        child: Text("${tile.data}"),
+    return Positioned(
+      // key: ValueKey(widget.tile.correctPos.x *
+      //         ref.read(BoardLogicController.provider).xDim +
+      //     widget.tile.correctPos.y),
+      // duration: const Duration(milliseconds: 250),
+      top: top,
+      left: left,
+      child: SizedBox(
+        width: widget.tileWidth,
+        height: widget.tileHeight,
+        child: GestureDetector(
+          onPanStart: (details) {
+            _movablePosition = ref
+                .read(BoardLogicController.provider)
+                .getMovablePosition(widget.tile);
+          },
+          onPanUpdate: (details) {
+            final delta = details.delta;
+
+            switch (_movablePosition) {
+              case MovablePosition.top:
+                if (top + delta.dy > top) {
+                  extraOffset += Offset(0, delta.dy);
+                }
+                break;
+              case MovablePosition.down:
+                if (top + delta.dy < top) {
+                  extraOffset += Offset(0, delta.dy);
+                }
+                break;
+              case MovablePosition.left:
+                if (left + delta.dx > left) {
+                  extraOffset += Offset(delta.dx, 0);
+                }
+                break;
+              case MovablePosition.right:
+                if (left + delta.dx < left) {
+                  extraOffset += Offset(delta.dx, 0);
+                }
+                break;
+              case MovablePosition.none:
+                // TODO: Handle this case.
+                break;
+            }
+            setState(() {});
+          },
+          onPanEnd: (details) {
+            if ((extraOffset.dx.abs() > widget.tileWidth / 2) ||
+                (extraOffset.dy.abs() > widget.tileHeight / 2)) {
+              ref.read(BoardAnimationController.provider).moveTile(widget.tile);
+            }
+            extraOffset = Offset.zero;
+            setState(() {});
+          },
+          onTap: () {
+            ref.read(BoardAnimationController.provider).moveTile(widget.tile);
+            // log("Can Move ${tile.data} ${board.canMoveTile(tile)}");
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              border: Border.all(color: Colors.blue),
+            ),
+            child: Center(
+              child: Text("${widget.tile.data}"),
+            ),
+          ),
+        ),
       ),
     );
   }
