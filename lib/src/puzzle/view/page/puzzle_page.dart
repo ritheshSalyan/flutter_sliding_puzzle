ff08fbfc634f2971cffd0bceedaf5821ebe61e02
ritheshSalyan <rithesh@dreamsoftin.com>
2022-02-06 21:40:15 +0530

    WIP on Wave

--- lib/src/puzzle/view/page/puzzle_page.dart
+++ lib/src/puzzle/view/page/puzzle_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sliding_puzzle/src/puzzle/view/widgets/depth_builder.dart';
 import 'package:sliding_puzzle/src/puzzle/view/widgets/input/keyboard_listner.dart';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
 
 import '../../puzzle.dart';
 
@@ -96,13 +98,55 @@ class Background extends HookConsumerWidget {
           ..translate(-100.0, -100.0, 0)
           ..translate(-offset.dx, -offset.dy, 0)
           ..scale(1.5),
-        child: Image.asset(
-          "assets/images/lava_a.jpg",
-          width: size.width * 1.5,
-          height: size.height * 1.5,
-          // fit: BoxFit.cover,
-          // fit: BoxFit.fill,
-          repeat: ImageRepeat.repeat,
+        child: WaveWidget(
+          config: CustomConfig(
+            gradients: [
+              [Colors.red, const Color(0xEEF44336).withOpacity(0.5)],
+              [
+                Colors.red.shade600.withOpacity(0.5),
+                const Color(0x77E57373).withOpacity(0.5)
+              ],
+              [
+                Colors.orange.withOpacity(0.5),
+                const Color(0x66FF9800).withOpacity(0.5)
+              ],
+              [
+                Colors.yellow.withOpacity(0.7),
+                const Color(0x55FFEB3B).withOpacity(0.5)
+              ]
+            ],
+            durations: [35000, 19440, 10800, 6000],
+            heightPercentages: [0.25, 0.2, 0.6, 1.0],
+            // blur: const MaskFilter.blur(BlurStyle.solid, 10),
+            gradientBegin: Alignment.bottomLeft,
+            gradientEnd: Alignment.topRight,
+          ),
+          // colors: [
+          //   Colors.white70,
+          //   Colors.white54,
+          //   Colors.white30,
+          //   Colors.white24,
+          // ],
+          // durations: [
+          //   32000,
+          //   21000,
+          //   18000,
+          //   5000,
+          // ],
+          // waveAmplitude: 0,
+          // heightPercentages: [0.25, 0.26, 0.28, 0.31],
+          backgroundImage: const DecorationImage(
+            image: AssetImage("assets/images/lava_a.jpg"
+                // 'https://images.unsplash.com/photo-1600107363560-a2a891080c31?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=672&q=80',
+                ),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.white, BlendMode.softLight),
+          ),
+          size: size,
+          // const Size(
+          //   double.infinity,
+          //   double.infinity,
+          // ),
         ),
       );
     });
