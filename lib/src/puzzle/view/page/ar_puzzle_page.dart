3fdb81979fa1805ab7a23c9df64b393d75c2808a
Rithesh <rithesh@dreamsoftin.com>
2022-02-23 17:46:04 +0530

    add Title section in AR Mode

--- lib/src/puzzle/view/page/ar_puzzle_page.dart
+++ lib/src/puzzle/view/page/ar_puzzle_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../puzzle.dart';
+import '../widgets/title_widget.dart';
 
 class ARPuzzlePage extends ConsumerWidget {
   const ARPuzzlePage({Key? key}) : super(key: key);
@@ -11,86 +12,27 @@ class ARPuzzlePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final screensize = MediaQuery.of(context).size;
     var sizedBox = const BoardView();
-    return DeferredPointerHandler(
-      child: ImageARWidget(
-          targetDB: TargetDB.asset("assets/marker/target_3.mind"),
-          tagets: [
-            ImageTarget(targetIndex: 0, children: [
-              WidgetTargetChild(
-                position: const TransformPosition(0.1, -0.1),
-                builder: (context, transform) {
-                  ref.read(BoardUIController.provider).rotateBoardTo(
-                      Offset(transform.rotation.y, transform.rotation.x));
-                  return SizedBox(width: screensize.width, child: sizedBox);
-                },
-              )
-            ])
-          ]),
-      // child: CommonScaffold(
-      //   small: (context, constraints) {
-      //     return Column(
-      //       children: [
-      //         InkWell(
-      //             onTap: () {
-      //               ref.read(BoardUIController.provider).triggerEndAnimation();
-      //             },
-      //             child: const TitleWidget()),
-      //         Expanded(
-      //           child: Center(
-      //             child: SizedBox(
-      //               // transform: Matrix4.identity()..translate(20.0, 20.0, 0),
-      //               // color: Colors.white,
-      //               width: (screensize.shortestSide * 0.75).clamp(100, 500.0),
-      //               height: (screensize.shortestSide * 0.75).clamp(100, 500.0),
-      //               child: const BoardView(),
-      //             ),
-      //           ),
-      //         )
-      //       ],
-      //     );
-      //   },
-      //   medium: (context, constraints) {
-      //     return CustomScrollView(
-      //       slivers: [
-      //         SliverList(
-      //             delegate: SliverChildListDelegate([
-      //           const TitleWidget(),
-      //         ])),
-      //         SliverFillRemaining(
-      //           child: Center(
-      //             child: SizedBox(
-      //               // transform: Matrix4.identity()..translate(20.0, 20.0, 0),
-      //               // color: Colors.white,
-      //               width: (screensize.shortestSide * 0.75).clamp(100, 500.0),
-      //               height: (screensize.shortestSide * 0.75).clamp(100, 500.0),
-      //               child: const BoardView(),
-      //             ),
-      //           ),
-      //         )
-      //       ],
-      //     );
-      //   },
-      //   large: (context, constraints) {
-      //     return Row(
-      //       crossAxisAlignment: CrossAxisAlignment.center,
-      //       children: [
-      //         const Expanded(child: TitleWidget()),
-      //         Expanded(
-      //           flex: 2,
-      //           child: Center(
-      //             child: SizedBox(
-      //               // transform: Matrix4.identity()..translate(20.0, 20.0, 0),
-      //               // color: Colors.white,
-      //               width: (screensize.shortestSide * 0.75).clamp(100, 500.0),
-      //               height: (screensize.shortestSide * 0.75).clamp(100, 500.0),
-      //               child: const BoardView(),
-      //             ),
-      //           ),
-      //         )
-      //       ],
-      //     );
-      //   },
-      // ),
+    return Material(
+      child: DeferredPointerHandler(
+        child: ImageARWidget(
+            targetDB: TargetDB.asset("assets/marker/target_3.mind"),
+            tagets: [
+              ImageTarget(targetIndex: 0, children: [
+                WidgetTargetNode.child(
+                    position: const TransformPosition(0.1, 0.4),
+                    child: Container(
+                        color: Colors.white, child: const TitleWidget())),
+                WidgetTargetNode(
+                  position: const TransformPosition(0.1, -0.1),
+                  builder: (context, transform) {
+                    ref.read(BoardUIController.provider).rotateBoardTo(
+                        Offset(transform.rotation.y, transform.rotation.x));
+                    return SizedBox(width: screensize.width, child: sizedBox);
+                  },
+                )
+              ])
+            ]),
+      ),
     );
   }
 }
